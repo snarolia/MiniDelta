e9f09c4b4eff9e667306dd825eee65a50df1745c
snarolia <siddharthnarolia@gmail.com>
2025-04-18 14:05:47 +0530

    creating delta table and logs

--- mini_delta.py
+++ mini_delta.py
@@ -2,3 +2,48 @@ import os
 import json
 import pandas as pd
 from datetime import datetime
+from uuid import uuid4
+
+
+class MiniDeltaTable:
+
+    ## Initialises the table path and log path
+    def __init__(self, table_path:str):
+        self.table_path=table_path
+        self.log_path=os.path.join(table_path, "_delta_log")
+        os.makedirs(self.log_path, exist_ok=True)
+    
+    ## Gets the list of existing logs and returns the name/number of next log (used for version controling)
+    def _get_next_version(self):
+        existing_logs=sorted([int(f.replace(".json", "")) for f in os.listdir(self.log_path) if f.endswith(".json")])
+        return (existing_logs[-1]+1) if existing_logs else 0 ## Returns the number/name of next log to be created
+    
+    def insert(self, df:pd.DataFrame):
+        version=self._get_next_version()
+        version_file=f"data_v_{version}.parquet"
+        df.to_parquet(os.path.join(self.table_path, version_file), index=False)
+
+        metadata={
+            "version":version,
+            "file":version_file,
+            "number_of_rows":len(df),
+            "schema":list(df.columns),
+            "timestamp":datetime.now().isoformat()
+        }
+        with open(os.path.join(self.log_path, f"{version}.json"), "w") as metadata_file:
+            json.dump(metadata, metadata_file, indent=2)
+    
+    def read(self, version=None):
+        if version is None:
+            version=self._get_next_version()-1
+        with open(os.path.join(self.log_path, f"{version}.json"), "r") as versiondata:
+            metadata=json.load(versiondata)
+        return pd.read_parquet(os.path.join(self.table_path, metadata["file"]))
+    
+    def getVersionHistory(self):
+        logs=[]
+        for f in os.listdir(self.log_path):
+            if f.endswith(".json"):
+                with open(os.path.join(self.log_path, f)) as jf:
+                    logs.append(json.load(jf))
+        return pd.DataFrame(logs)
